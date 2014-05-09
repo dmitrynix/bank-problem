d7576252fbb438f3bf1422a11ffeb9afb1638702
Dmitry Nix <dmitry.nix@gmail.com>
2014-05-09 13:01:22 -0300

    Add specs to "next" steps.

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -40,4 +40,36 @@ describe Bank do
       expect(subject.calc).to eq 1
     end
   end
+
+  context "calcule" do
+    subject do
+      Bank.new :cashs => 3, :clients_count => 16
+    end
+
+    before do
+      subject.add_cliente Client.new(3, 16)
+      subject.add_cliente Client.new(0, 10)
+      subject.add_cliente Client.new(0, 10)
+      subject.add_cliente Client.new(0, 10)
+      subject.add_cliente Client.new(3, 10)
+      subject.add_cliente Client.new(5, 10)
+      subject.add_cliente Client.new(7, 10)
+      subject.add_cliente Client.new(11, 10)
+      subject.add_cliente Client.new(13, 10)
+      subject.add_cliente Client.new(14, 10)
+      subject.add_cliente Client.new(15, 10)
+      subject.add_cliente Client.new(16, 10)
+      subject.add_cliente Client.new(17, 10)
+      subject.add_cliente Client.new(18, 3)
+      subject.add_cliente Client.new(19, 10)
+      subject.add_cliente Client.new(20, 10)
+      subject.add_cliente Client.new(23, 3)
+    end
+
+    it "calcule" do
+      pending "the code is prepared to only one cash" do
+        expect(subject.calc).to eq 2
+      end
+    end
+  end
 end
